6f727100666986ed21bf9f2f15b150346d1932e5
Forge <forge@forge-runtime.dev>
2026-07-01 21:27:47 +0000

    forge: 1

--- hello.py
+++ hello.py
@@ -1,15 +1,1 @@
-"""Hello module."""
-
-
-def hello() -> str:
-    """Return greeting message."""
-    return "Hello World"
-
-
-def main() -> None:
-    """Print greeting."""
-    print(hello())
-
-
-if __name__ == "__main__":
-    main()
+print('Hello World')
